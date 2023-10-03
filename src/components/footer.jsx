f92db489080204fae931f0f8f9f735c371bc47f4
Afrizal Yogi <xxgdev@gmail.com>
2023-10-03 07:50:59 +0700

    Add Navbar and Footer Base

---
+++ src/components/footer.jsx
@@ -0,0 +1,29 @@
+export default function Footer() {
+	return (
+		<footer className="fixed-bottom">
+			<div className="container footer-content">
+				<div className="row">
+					<div className="col">
+						<h4>
+							<a className="fw-bold" href="#">
+								Bijakcuan.
+							</a>
+						</h4>
+						<p>Pelatihan Keuangan untuk Sukses Finansial</p>
+					</div>
+					<div className="col"></div>
+					<div className="col"></div>
+					<div className="col"></div>
+					<div className="col"></div>
+				</div>
+			</div>
+			<div className="border-top">
+				<div className="container">
+					<p className="pt-4 pb-2">
+						© 2023 <b>Bijak Cuan</b>. All Rights Reserved.
+					</p>
+				</div>
+			</div>
+		</footer>
+	)
+}
