88b4e6efb832184c8637099a2de136f8120e6890
Afrizal Yogi <xxgdev@gmail.com>
2023-10-15 13:54:16 +0700

    Change Checkout to Client

--- src/components/generate-order-id.jsx
+++ src/components/generate-order-id.jsx
@@ -1,9 +1,6 @@
 export default function generateOrderId() {
 	const currentDate = new Date()
-	const year = currentDate.getFullYear()
-	const month = (currentDate.getMonth() + 1).toString().padStart(2, "0")
-	const day = currentDate.getDate().toString().padStart(2, "0")
 	const timestamp = new Date().getTime()
 	const random = Math.floor(Math.random() * 90 + 10)
-	return `ORDER${year}${month}${day}${timestamp}${random}`
+	return `ORDER${timestamp}${random}`
 }
