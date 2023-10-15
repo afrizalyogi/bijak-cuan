036445f117e4908dd45a598fcb434857e3e8909f
Afrizal Yogi <xxgdev@gmail.com>
2023-10-15 13:34:45 +0700

    Update Async Data Refresh

--- src/app/checkout/pembayaran/gagal/page.jsx
+++ src/app/checkout/pembayaran/gagal/page.jsx
@@ -2,8 +2,13 @@ import "../pembayaran.css"
 import Link from "next/link"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTimesCircle } from "@fortawesome/free-solid-svg-icons"
+import { useRouter } from "next/navigation"
 
 export default async function PembayaranGagal() {
+	const router = useRouter()
+	const handleRefresh = () => {
+		router.reload()
+	}
 	return (
 		<main id="pembayaran">
 			<div className="container align-items-center d-flex justify-content-center">
@@ -15,7 +20,10 @@ export default async function PembayaranGagal() {
 					<p className="mb-5 w-75 mx-auto">
 						Silahkan lakukan pembayaran pesanan Anda
 					</p>
-					<Link href={"/program"} className="btn btn-primary">
+					<Link
+						href={"/program"}
+						onClick={handleRefresh}
+						className="btn btn-primary">
 						Kembali
 					</Link>
 				</div>
